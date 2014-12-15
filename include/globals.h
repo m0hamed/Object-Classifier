eab53f3af54f25ea490aeeb5206302271063b23e
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2014-12-15 14:10:25 +0200

    Added global includes and constansts file

---
+++ include/globals.h
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  globals.h
+ *
+ *    Description:  global constants and includes
+ *
+ *        Version:  1.0
+ *        Created:  12/15/2014 11:37:05 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mohamed Ashraf (m0hamed)
+ *   Organization:  GUC
+ *
+ * =====================================================================================
+ */
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using cv::Mat;
+
+#endif
