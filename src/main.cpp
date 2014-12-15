e9c18036286e9fff482137f44a47f63c7b39706a
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2014-12-15 18:19:23 +0200

    Added dense sift to main

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,26 @@
  * =====================================================================================
  */
 
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include "globals.h"
 #include "file_utils.h"
+#include "sift_utils.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 using cv::imread;
+using cv::Size;
 
 int main() {
   vector<string> car_images = get_files_in_directory(CAR_IMAGES_PATH);
   int image_index = 0;
-  for(string image : car_images) {
-    Mat image = imread(image, 0);
+  for(string image_path : car_images) {
+    Mat image = imread(image_path, 0);
+    equalizeHist(image, image);
+    Mat image_resized;
+    resize(image, image_resized, Size(300, 250));
+    cv::SiftDescriptorExtractor extractor;
+    Mat sift_descriptors;
+    vector<KeyPoint> keypoints = create_dense_keypoints(300, 250, 5);
+    extractor.compute(image_resized, keypoints, sift_descriptors);
   }
   return 0;
 }
